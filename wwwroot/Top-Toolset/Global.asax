359774082a06b1b96236738332f71bb767f03509
CharlieHutchinsonTheMagicalRabbit <charliehutchinson85@googlemail.com>
2016-01-28 08:38:50 +0000

    Revert "Revert "Commit1""
    This reverts commit ce8db4d3bcc9e3c8672ba4bad6f831b33622d809.

---
+++ wwwroot/Top-Toolset/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Top_Toolset.MvcApplication" Language="C#" %>
---
+++ wwwroot/Top-Toolset/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Top_Toolset.Startup))]
+namespace Top_Toolset
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
